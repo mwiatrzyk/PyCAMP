91b14be29f36703cc6d3d93e9e6089b448ad1d69
Maciej Wiatrzyk <maciej.wiatrzyk@gmail.com>
2011-05-12 22:37:51 +0200

    add OCR plugins base class + Tesseract OCR plugin

---
+++ camp/plugins/ocr/__init__.py
@@ -0,0 +1,84 @@
+import os
+
+from subprocess import Popen, PIPE
+
+
+class OCRPluginBase(object):
+    """Base class for external OCR plugins.
+    
+    :attr __ocr_command__: command used to execute external OCR tool. Can have
+        ``%(foo)s`` like substitutions"""
+    __ocr_command__ = None
+
+    def __init__(self, working_dir):
+        """Create new instance of OCR recognitor.
+        
+        :param working_dir: specifies working directory"""
+        super(OCRPluginBase, self).__init__()
+        self.working_dir = working_dir
+        if not os.path.isdir(self.working_dir):
+            os.makedirs(self.working_dir)
+    
+    def execute(self, **params):
+        """Execute external OCR application by executing
+        :attr:`__ocr_command__` command. Optional parameters are used to
+        replace ``%(foo)s``-like template variables in provided command.
+        Returns ``True`` if execution succeeds or ``False`` otherwise.
+        
+        :rtype: bool"""
+        cmd = self.__class__.__ocr_command__
+        p = Popen(
+            cmd % params if params else cmd,
+            shell=True, bufsize=1024,
+            stdout=PIPE, stderr=PIPE, close_fds=True)
+        if p.wait() != 0:
+            return False
+        return True
+
+    def create_infile(self, segment, angle=None):
+        """Creates input file for external OCR tool. Return value will be used
+        as :param:`infile` parameter of :meth:`get_result`.
+        
+        :param segment: a segment to be recognized
+        :param angle: specifies rotation angle. Usefull when vertical text
+            segments need to be recognized as well"""
+        raise NotImplementedError()
+
+    def get_result(self, segment, infile):
+        """Execute :attr:`__ocr_command__` command in subprocess and await for
+        it to finish. Return value will be used as return value of
+        :meth:`perform` and as argument of :meth:`recognized`.
+        
+        :param segment: a segment to be recognized
+        :param infile: input file. This is result of :meth:`create_infile`"""
+        raise NotImplementedError()
+    
+    def recognized(self, result):
+        """Used to check if result produced by external OCR tool is correct (is
+        a meaningful text, not a mess).
+        
+        :param result: return value of :meth:`get_result`"""
+        raise NotImplementedError()
+
+    def perform(self, segment, angles=None):
+        """Begin text recognition process on given segment taken from image.
+        
+        :param angles: sequence of rotation angles. If given, recognition
+            process is repeated up to ``len(angles)`` times and the segment is
+            rotated by the current angle before recognition process starts.
+            Loop is terminated once text is recognized or once there are no
+            more angles to use."""
+        for a in (angles or [None]):
+            infile = self.create_infile(segment, angle=a)
+            result = self.get_result(segment, infile)
+            if self.recognized(result):
+                return result
+
+
+def load_plugin(name):
+    """Load OCR plugin of given name and return the plugin class.
+    
+    :rtype: subclass of :class:`OCRPluginBase`"""
+    clsname = name.title()
+    module = __import__("camp.plugins.ocr.%s" % name, fromlist=[clsname])
+    return getattr(module, clsname)
---
+++ camp/plugins/ocr/tesseract.py
@@ -0,0 +1,20 @@
+import os
+
+from camp.plugins.ocr import OCRPluginBase
+
+
+class Tesseract(OCRPluginBase):
+    __ocr_command__ = 'tesseract %(infile)s %(outfile)s'
+
+    def create_infile(self, segment, angle=None):
+        infile = os.path.join(self.working_dir, '%d_%d_%d_%d.tif' % segment.bounds)
+        segment.toimage(border=2, angle=angle).save(infile)
+        return infile
+    
+    def get_result(self, segment, infile):
+        outfile = os.path.join(self.working_dir, '%d_%d_%d_%d' % segment.bounds)
+        if self.execute(infile=infile, outfile=outfile):
+            return open("%s.txt" % outfile).read().strip()
+    
+    def recognized(self, result):
+        return result and '\n' not in result
