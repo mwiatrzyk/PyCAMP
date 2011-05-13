6dd97f801bfbe3678bcf4ebd9450747d1cdfb254
Maciej Wiatrzyk <maciej.wiatrzyk@gmail.com>
2011-05-13 13:41:00 +0200

    catching exceptions and using logging.exception to display them

--- run.py
+++ run.py
@@ -7,4 +7,9 @@ from camp.app import Application
 
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 
-sys.exit(Application.instance('config.ini').run(*sys.argv[1:]))
+try:
+    status = Application.instance('config.ini').run(*sys.argv[1:])
+    sys.exit(status)
+except Exception:
+    logging.exception('an error occured:')
+    sys.exit(1)
